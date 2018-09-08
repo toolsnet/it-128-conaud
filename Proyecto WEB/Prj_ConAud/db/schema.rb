48ff9a3cb121756e0cf60a0ac86d2b5fbe86216c
Camt <camt@pp.com>
2018-09-08 16:32:36 -0500

    se actualiza proyecto
    se actualiza proyecto

--- Proyecto WEB/Prj_ConAud/db/schema.rb
+++ Proyecto WEB/Prj_ConAud/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_08_184420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,18 @@ ActiveRecord::Schema.define(version: 0) do
 
   create_table "tip_doc", primary_key: "id_tip_doc", id: :serial, force: :cascade do |t|
     t.string "nom_tip_doc", limit: 20
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "autoridad", "tip_doc", column: "id_tip_doc", primary_key: "id_tip_doc", name: "autoridad_id_tip_doc_fkey"
