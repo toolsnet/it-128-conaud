ebb617d1d5a60052e02713ee16d57fa205541b77
Camt <camt@pp.com>
2018-09-15 16:10:02 -0500

    Actualizacion
    Se inicia la instalacion del token

--- Proyecto WEB/Prj_ConAud/config/routes.rb
+++ Proyecto WEB/Prj_ConAud/config/routes.rb
@@ -12,3 +12,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#home'
 end
+
+Rails.application.routes.draw do
+  devise_for :users
+  root 'welcome#index'
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      devise_scope :user do
+        post '/authentication_tokens/create', to: "authentication_tokens#create"
+      end
+    end
+  end
+
