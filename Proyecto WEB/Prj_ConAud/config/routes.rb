4c374bbbb251eb7625a900de5a1ae53c5f241bae
Camilo Tiriat <camtiriat@gmail.com>
2018-09-16 09:36:50 -0700

    Correccion sintexis routes
    Correccion sintexis routes

--- Proyecto WEB/Prj_ConAud/config/routes.rb
+++ Proyecto WEB/Prj_ConAud/config/routes.rb
@@ -11,17 +11,14 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#home'
-end
-
-Rails.application.routes.draw do
-  devise_for :users
-  root 'welcome#index'
+   
   namespace :api do
     namespace :v1 do
       resources :users
       devise_scope :user do
         post '/authentication_tokens/create', to: "authentication_tokens#create"
+     end
       end
-    end
-  end
+   end
+end
 
